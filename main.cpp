bf7fbd664d368fc2b0ff668b88369a3fa2b98194
karokotlowska <62810585+karokotlowska@users.noreply.github.com>
2022-12-04 19:53:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 
-#include <stdlib.h>   // standard library (set of standard C functions
-#include <glut.h>  // glut (gl utility toolkit) basic windows functions, keyboard, mouse.
-#include <stdio.h>    // standard (I/O library)
-#include <math.h>     // Math library (Higher math functions )
+#include <stdlib.h>   
+#include <glut.h>  
+#include <stdio.h>    
+#include <math.h>     
 #include <string.h>
 #include <windows.h>    
 #include <io.h>
@@ -16,204 +16,84 @@ GLfloat LightDiffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
 GLfloat LightPosition[] = { 5.0f, 25.0f, 5.0f, 1.0f };
 GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 
-// mouse variables: Win = windows size, mouse = mouse position
-int mouse_x, mouse_y, Win_x, Win_y, object_select;
+int mouse_x, mouse_y, Win_x, Win_y, coordinates_clicked;
 
-// state variables for Orho/Perspective view, lighting on/off
-static int view_state = 0, light_state = 0;
+int spin, elements_to_spin;
 
-// Use to spin X's and O's
-int spin, spinboxes;
+//result = 1 -> player 1 wins
+//result = -1 -> player 2 wins
+//result = 2 -> draw
+int player_turn, result, start_game;
 
-int playerturn;
+void* font = GLUT_BITMAP_HELVETICA_18;
 
-// Win = 1 player wins, -1 computer wins, 2 tie.
-// player or computer; 1 = X, -1 = O
-// start_game indicates that game is in play.
-int player, computer, result, start_game;
+static int box[8][3] = { 
+	{0, 1, 2}, 
+	{3, 4, 5}, 
+	{6, 7, 8}, 
+	{0, 3, 6},		
+	{1, 4, 7}, 
+	{2, 5, 8}, 
+	{0, 4, 8}, 
+	{2, 4, 6} 
+};
 
+// 1 = x, -1 = O, 0 = nothing
+int boxes[9];
 
-// alingment of boxes in which one can win
-// We have 8 posiblities, 3 accross, 3 down and 2 diagnally
-//
-// 0 | 1 | 2
-// 3 | 4 | 5
-// 6 | 7 | 8
-//
-// row, colunm, diagnal information
-
-static int box[8][3] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6},
-				{1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6} };
-
-// Storage for our game board
-// 1 = X's, -1 = O's, 0 = open space
-
-int box_map[9];
-// center x,y location for each box
+//centers
 int object_map[9][2] = { {-6,6},{0,6},{6,6},{-6,0},{0,0},{6,0},{-6,-6},{0,-6},{6,-6} };
-
-// quadric pointer for build our X
-GLUquadricObj* Cylinder;
 
 void init_game(void)
 {
 	int i;
 
-	// Clear map for new game
+	// clear matrix
 	for (i = 0; i < 9; i++) {
-		box_map[i] = 0;
+		boxes[i] = 0;
 	}
 
 	result = 0;
 	start_game = 1;
-	playerturn = 1; //x starts first
+	player_turn = 1;
 }
 
-
-// Check for three in a row/colunm/diagnal
-// returns 1 if there is a winner
-int check_move(void) {
-
+int check_if_draw(void) {
 	int i, t = 0;
 
-	//Check for three in a row
 	for (i = 0; i < 8; i++) {
-		t = box_map[box[i][0]] + box_map[box[i][1]] + box_map[box[i][2]];
-		if ((t == 3) || (t == -3)) {
-			spinboxes = i;
-			return(1);
-		}
-	}
-	t = 0;
-	// check for tie
-	for (i = 0; i < 8; i++)
-	{
-		t = t + abs(box_map[box[i][0]]) + abs(box_map[box[i][1]]) + abs(box_map[box[i][2]]);
+		t = t + abs(boxes[box[i][0]]) + abs(boxes[box[i][1]]) + abs(boxes[box[i][2]]);
 	}
 
 	if (t == 24) return(2);
 
-	return(0);
+	return 0;
 }
 
-// Do we need to block other player?
-int blocking_win(void)
-{
-	int i, t;
-	for (i = 0; i < 8; i++)
-	{
-		t = box_map[box[i][0]] + box_map[box[i][1]] + box_map[box[i][2]];
-		if ((t == 2) || (t == -2))
-		{
-			// Find empty
-			if (box_map[box[i][0]] == 0) box_map[box[i][0]] = computer;
-			if (box_map[box[i][1]] == 0) box_map[box[i][1]] = computer;
-			if (box_map[box[i][2]] == 0) box_map[box[i][2]] = computer;
+int check_move(void) {
+	int i, t = 0;
+
+	for (i = 0; i < 8; i++) {
+		t = boxes[box[i][0]] + boxes[box[i][1]] + boxes[box[i][2]];
+		if ((t == 3) || (t == -3)) {
+			elements_to_spin = i;
 			return(1);
 		}
 	}
-	return(0);
+	
+	return(check_if_draw());
 }
 
-// check for a free space in corner
-int check_corner(void)
-{
-	int i;
-
-	if (box_map[0] == 0)
-	{
-		box_map[0] = computer;
-		i = 1;
-		return(1);
-	}
-
-	if (box_map[2] == 0)
-	{
-		box_map[2] = computer;
-		i = 1;
-		return(1);
-	}
-
-	if (box_map[6] == 0)
-	{
-		box_map[6] = computer;
-		i = 1;
-		return(1);
-	}
-
-	if (box_map[8] == 0)
-	{
-		box_map[8] = computer;
-		i = 1;
-		return(1);
-	}
-
-	return(0);
-}
-
-// Check for free space in row
-int check_row(void)
-{
-
-	if (box_map[4] == 0)
-	{
-		box_map[4] = computer;
-		return(1);
-	}
-
-	if (box_map[1] == 0)
-	{
-		box_map[1] = computer;
-		return(1);
-	}
-
-	if (box_map[3] == 0)
-	{
-		box_map[3] = computer;
-		return(1);
-	}
-	if (box_map[5] == 0)
-	{
-		box_map[5] = computer;
-		return(1);
-	}
-	if (box_map[7] == 0)
-	{
-		box_map[7] = computer;
-		return(1);
-	}
-
-	return(0);
-}
-
-// logic for computer's turn
-int computer_move()
-{
-	if (blocking_win() == 1) return(1);
-	if (check_corner() == 1) return(1);
-	if (check_row() == 1) return(1);
-
-	return(0);
-}
-
-// I use this to put text on the screen
-void printResult(int x, int y, char* st)
-{
-	int l, i;
-
-	l = strlen(st); // see how many characters are in text string.
-	glRasterPos2i(x, y); // location to start printing text
-	for (i = 0; i < l; i++)  // loop until i is less then l
-	{
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, st[i]); // Print a character on the screen
-	}
-
+void printString(char* s, float x, float y) {
+	unsigned int i;
+	glRasterPos2f(x, y);
+	for (i = 0; i < strlen(s); i++)
+		glutBitmapCharacter(font, s[i]);
 }
 
 
 // This creates the spinning of the cube.
-static void TimeEvent(int te)
-{
+static void TimeEvent(int te) {
 
 	spin++;  // increase cube rotation by 1
 	if (spin > 360) spin = 0; // if over 360 degress, start back at zero.
@@ -222,124 +102,81 @@ static void TimeEvent(int te)
 }
 
 
-// Setup our Opengl world, called once at startup.
-void init(void)
-{
-	glClearColor(1.0, 1.0, 1.0, 0.0);  // When screen cleared, use black.
-	glShadeModel(GL_SMOOTH);  // How the object color will be rendered smooth or flat
-	glEnable(GL_DEPTH_TEST);   // Check depth when rendering
-	// Lighting is added to scene
+void init() {
+	glClearColor(1.0, 1.0, 1.0, 0.0);  
+	glEnable(GL_DEPTH_TEST);   
+
 	glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);
 	glLightfv(GL_LIGHT1, GL_POSITION, LightPosition);
-	glEnable(GL_LIGHTING);  // Turn on lighting
-	glEnable(GL_LIGHT1);    // Turn on light 1
+
+	glEnable(GL_LIGHTING); 
+	glEnable(GL_LIGHT1);    
 
 	start_game = 0;
 	result = 0;
-
-	// Create a new quadric
-	Cylinder = gluNewQuadric();
-	gluQuadricDrawStyle(Cylinder, GLU_FILL);
-	gluQuadricNormals(Cylinder, GLU_SMOOTH);
-	gluQuadricOrientation(Cylinder, GLU_OUTSIDE);
 }
 
 
 void Draw_O(int x, int y, int z, int a)
 {
-
+	glColor3f(0.0, 0.0, 1.0);
 	glPushMatrix();
 	glTranslatef(x, y, z);
-	glRotatef(a, 1, 0, 0);
-	glutSolidTorus(0.5, 2.0, 8, 16);
+	glRotatef(a, 1, 1, 1);
+	glutSolidTorus(0.7, 2.0, 8, 16);
 	glPopMatrix();
-
 }
 
 
 void Draw_X(int x, int y, int z, int a)
 {
-
+	glColor3f(1.0, 1.0, 0.0);
 	glPushMatrix();
 	glTranslatef(x, y, z);
+
 	glPushMatrix();
-	glRotatef(a, 1, 0, 0);
+	glRotatef(a, 1, 1, 1);
 	glRotatef(90, 0, 1, 0);
 	glRotatef(45, 1, 0, 0);
-	glTranslatef(0, 0, -3);
-	gluCylinder(Cylinder, 0.5, 0.5, 6, 16, 16);
-	//glutSolidCone( 2.5, 3.0, 16, 8 );
+	glTranslatef(0, 0, -2.5);
+	glutSolidCone(1, 2.5, 9, 8);
 	glPopMatrix();
+
 	glPushMatrix();
-	glRotatef(a, 1, 0, 0);
+	glRotatef(a, 1, 1, 1);
 	glRotatef(90, 0, 1, 0);
 	glRotatef(315, 1, 0, 0);
-	glTranslatef(0, 0, -3);
-	gluCylinder(Cylinder, 0.5, 0.5, 6, 16, 16);
-	//glutSolidCone( 2.5, 3.0, 16, 8 );
+	glTranslatef(0, 0, -2.5);
+	glutSolidCone(1, 2.5, 9, 8);
 	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(a, 1, 1, 1);
+	glRotatef(90, 0, 1, 0);
+	glRotatef(135, 1, 0, 0);
+	glTranslatef(0, 0, -2.5);
+	glutSolidCone(1, 2.5, 9, 8);
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(a, 1, 1, 1);
+	glRotatef(90, 0, 1, 0);
+	glRotatef(225, 1, 0, 0);
+	glTranslatef(0, 0, -2.5);
+	glutSolidCone(1, 2.5, 9, 8);
+	glPopMatrix();
+
 	glPopMatrix();
 
 }
 
-// Draw our world
-void display(void)
-{
+void draw_grid() {
 	int ix, iy;
-	int i;
-	int j;
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  //Clear the screen
-
-	glMatrixMode(GL_PROJECTION);  // Tell opengl that we are doing project matrix work
-	glLoadIdentity();  // Clear the matrix
-	glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0);  // Setup an Ortho view
-	glMatrixMode(GL_MODELVIEW);  // Tell opengl that we are doing model matrix work. (drawing)
-	glLoadIdentity(); // Clear the model matrix
-
-	glDisable(GL_COLOR_MATERIAL);
-	glDisable(GL_LIGHTING);
-	glColor3f(0.0, 1.0, 1.0);
-	if (result == 1) printResult(-1, 0, (char*)"You win");
-	if (result == -1) printResult(-1, 0, (char*)"Computer win");
-	if (result == 2) printResult(-1, 0, (char*)"       It's a draw");
-
-	// Setup view, and print view state on screen
-	if (view_state == 1)
-	{
-		glColor3f(0.0, 0.0, 1.0);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		gluPerspective(60, 1, 1, 30);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	}
-	else
-	{
-		glColor3f(0.0, 0.0, 1.0);
-	}
-	// Lighting on/off
-	if (light_state == 1)
-	{
-		glDisable(GL_LIGHTING);
-		glDisable(GL_COLOR_MATERIAL);
-	}
-	else
-	{
-		glEnable(GL_LIGHTING);
-		glEnable(GL_COLOR_MATERIAL);
-	}
-
-
-	gluLookAt(0, 0, 20, 0, 0, 0, 0, 1, 0);
-
-
-	// Draw Grid
-	for (ix = 0; ix <= 3; ix++)
-	{
+	for (ix = 0; ix <= 3; ix++) {
 		glPushMatrix();
-		glLineWidth(4.0);
+		glLineWidth(14.0);
 		glColor3f(0, 0, 1);
 		glBegin(GL_LINES);
 		glVertex2i(-9, -9 + ix * 6);
@@ -347,10 +184,9 @@ void display(void)
 		glEnd();
 		glPopMatrix();
 	}
-	for (iy = 0; iy <= 3; iy++)
-	{
+	for (iy = 0; iy <= 3; iy++) {
 		glPushMatrix();
-		glLineWidth(4.0);
+		glLineWidth(14.0);
 		glColor3f(0, 0, 1);
 		glBegin(GL_LINES);
 		glVertex2i(-9 + iy * 6, 9);
@@ -358,41 +194,58 @@ void display(void)
 		glEnd();
 		glPopMatrix();
 	}
+}
+
+void display() {
+	int i, j;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  //Clear the screen
+
+	glMatrixMode(GL_PROJECTION);  // Tell opengl that we are doing project matrix work
+	glLoadIdentity();  // Clear the matrix
+	glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 130.0);  // Setup an Ortho view
+	glMatrixMode(GL_MODELVIEW);  // Tell opengl that we are doing model matrix work. (drawing)
+	glLoadIdentity(); // Clear the model matrix
+
+	glDisable(GL_COLOR_MATERIAL);
+	glDisable(GL_LIGHTING);
+	glColor3f(0.0, 1.0, 1.0);
+	if (result == 1) printString((char*)"player 1 wins", -1, 0);
+	if (result == -1) printString( (char*)"player 2 wins", -1, 0);
+	if (result == 2) printString( (char*)"It's a draw!", -1, 0);
+
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_COLOR_MATERIAL);
+
+	gluLookAt(0, 0, 25, 0, 0, 0, 0, 1, 0);
+
+	draw_grid();
 
 	glColorMaterial(GL_FRONT, GL_AMBIENT);
 	glColor4f(0.5, 0.5, 0.5, 1.0);
 	glColorMaterial(GL_FRONT, GL_EMISSION);
 	glColor4f(0.0, 0.0, 0.0, 1.0);
 	glColorMaterial(GL_FRONT, GL_SPECULAR);
-	glColor4f(0.35, 0.35, 0.35, 1.0);
+	glColor4f(0.0, 0.35, 0.0, 1.0);
 	glColorMaterial(GL_FRONT, GL_DIFFUSE);
-	glColor4f(0.69, 0.69, 0.69, 1.0);
-	//glDisable(GL_COLOR_MATERIAL);
-	glColor3f(1.0, 1.0, 1.0);  // Cube color
-	//glEnable(GL_COLOR_MATERIAL);
-	// Draw object in box's
+	glColor4f(0.8, 0.8, 0.0, 1.0);
 
 	for (i = 0; i < 9; i++) {
 		j = 0;
-		if (abs(result) == 1)
-		{
-			if ((i == box[spinboxes][0]) || (i == box[spinboxes][1]) || (i == box[spinboxes][2]))
-			{
+		if (abs(result) == 1) {
+			if ((i == box[elements_to_spin][0]) || (i == box[elements_to_spin][1]) || (i == box[elements_to_spin][2])) {
 				j = spin;
 			}
 			else j = 0;
 		}
-		if (box_map[i] == 1) Draw_X(object_map[i][0], object_map[i][1], -1, j);
-
-		if (box_map[i] == -1) Draw_O(object_map[i][0], object_map[i][1], -1, j);
+		if (result == 2) j = spin;
+		if (boxes[i] == 1) Draw_X(object_map[i][0], object_map[i][1], -1, j);
+		if (boxes[i] == -1) Draw_O(object_map[i][0], object_map[i][1], -1, j);
 	}
-
-	//glDisable(GL_COLOR_MATERIAL);
-
 	glutSwapBuffers();
 }
 
-// This is called when the window has been resized.
 void windowResize(int w, int h)
 {
 	Win_x = w;
@@ -402,22 +255,6 @@ void windowResize(int w, int h)
 	glLoadIdentity();
 }
 
-void play_x()
-{
-	//select=1;
-	player = 1;
-	computer = -1;
-	init_game();
-}
-void play_y()
-{
-	player = -1;
-	computer = 1;
-	init_game();
-	computer_move();
-}
-
-
 void click(int button, int state, int x, int y)
 {
 	// We convert windows mouse coords to out openGL coords
@@ -425,91 +262,86 @@ void click(int button, int state, int x, int y)
 	mouse_y = (18 * (float)((float)y / (float)Win_y)) / 6;
 
 	// What square have they clicked in?
-	object_select = mouse_x + mouse_y * 3;
+	coordinates_clicked = mouse_x + mouse_y * 3;
 
-	if (start_game == 0)
-	{
-		if ((button == GLUT_RIGHT_BUTTON) && (state == GLUT_DOWN))
-		{
-			player = 1;
-			computer = -1;
-			init_game();
-			return;
-		}
-
-		if ((button == GLUT_LEFT_BUTTON) && (state == GLUT_DOWN))
-		{
-			player = -1;
-			computer = 1;
-			init_game();
-			computer_move();
-			return;
+	if (start_game == 0) {
+		if ((button == GLUT_LEFT_BUTTON) && (state == GLUT_DOWN)) {
+			if (player_turn == 1) {
+				player_turn = -1;
+				init_game();
+				return;
+			}
+			else {
+				player_turn = 1;
+				init_game();
+				return;
+			}
+			
 		}
 	}
 
-	if (start_game == 1)
-	{
+	if (start_game == 1) {
 		if ((button == GLUT_LEFT_BUTTON) && (state == GLUT_DOWN)) {
+			if (player_turn == 1) {
+				player_turn = -1;
+			}
+			else {
+				player_turn = 1;
+			}
 			if (result == 0) {
-				if (box_map[object_select] == 0) {
-					box_map[object_select] = player;
+				if (boxes[coordinates_clicked] == 0) {
+					boxes[coordinates_clicked] = player_turn;
 					result = check_move();
 					if (result == 1) {
 						start_game = 0;
 						return;
-					}
-					computer_move();
-					result = check_move();
-					if (result == 1) {
-						result = -1;
-						start_game = 0;
 					}
 				}
 			}
 		}
 	}
 
-	if (result == 2)start_game = 0;
+	if (result == 2) {
+		start_game = 0;
+	}
 
 }
-void options(int id) {
-	switch (id) {
 
+void menu(int id) {
+	switch (id) {
 	case 1:
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		play_x();
+		init_game();
 		break;
 	case 2:
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		play_y();
-		break;
-	case 3:
 		exit(0);
 	}
 }
-void keyboard(unsigned char k, int i, int j)
-{
+void keyboard(unsigned char k, int i, int j) {
 	switch (k) {
-	case 'Q':
-	case 'q': exit(0);
-	default:break;
+	case 'r': 
+		init_game(); 
+		break;
+	case 'q': 
+		exit(0);
+	default: 
+		break;
 	}
 }
 
-// Main program
-void main()
-{
+void main() {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(768, 768);
 	glutInitWindowPosition(10, 10);
 	glutCreateWindow("Tic Tac Toe");
 	glutSetWindowTitle("Tic Tac Toe 3D");
 	init();
-	glutCreateMenu(options);
-	glutAddMenuEntry("Play with X", 1);
-	glutAddMenuEntry("Play with O", 2);
-	glutAddMenuEntry("Quit", 3);
+
+	glutCreateMenu(menu);
+	glutAddMenuEntry("Start again", 1);
+	glutAddMenuEntry("Exit", 2);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
 	glutDisplayFunc(display);
 	glutReshapeFunc(windowResize);
 	glutKeyboardFunc(keyboard);
